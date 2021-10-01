efa35a93c8d413e3875e90ee8972c97c951dbbd6
ac-dvlpr <54845674+ac-dvlpr@users.noreply.github.com>
2021-10-01 17:03:54 +0200

    Added the webpage files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var user = 'agataciekanowska';
+var domain = 'gmail.com';
+var icon = '<i class="far fa-envelope"></i>';
+var email = document.querySelector("footer");
+email.innerHTML += '<a hr' + 'ef="mai' + 'lto:' + user + '\x40' + domain + '">' + icon + '<'+'/a>';
